a568e56970be8abf6c4bb2a8db05fea476f26693
katataku <katataku.dev@gmail.com>
2022-04-02 16:20:32 +0900

    init

--- main.c
+++ main.c
@@ -6,32 +6,11 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 16:09:06 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/04/02 16:19:38 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/04/02 16:20:25 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-/*
- * ^Cは表示されてしまう
- */
-void	sigint_handler(int signal)
-{
-	(void)signal;
-	ft_putchar_fd('\n', STDOUT_FILENO);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-}
-
-/*
- * SIGQUITは特に何もしなくても無視されていそう　
- */
-void	set_signal(void)
-{
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGINT, sigint_handler);
-}
 
 int	main(int argc, char **argv, char **env)
 {
