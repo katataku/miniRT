877f5164d3d20368226f2814cab1423878d032f2
katataku <katataku.dev@gmail.com>
2022-05-15 18:26:40 +0900

    構造体を追加

--- includes/define.h
+++ includes/define.h
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 12:33:34 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/03/10 12:24:31 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/05/15 18:24:10 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,13 @@
 
 # include <stdbool.h>
 # include <stdio.h>
-# define READ_INDEX 0
-# define WRITE_INDEX 1
+# include "math.h"
+# include "vector3.h"
 
 # define STATUS_SUCCESS 0
 # define STATUS_FAILURE 1
 # define STATUS_MISUSE_BUILTIN 2
 # define STATUS_CAN_NOT_EXECUTE 126
 # define STATUS_COMMAND_NOT_FOUND 127
-
-extern int	g_last_exit_status;
-
-enum e_token
-{
-	T_NOTUSE,
-	T_WORD,
-	T_BAR,
-	T_GT,
-	T_GTGT,
-	T_LT,
-	T_LTLT,
-};
-
-typedef struct s_token
-{
-	int		*token;
-	char	**word;
-}	t_token;
-
-typedef struct s_exec_info
-{
-	char	***cmds;
-	int		cmd_num;
-	char	*srcfile;
-	char	*heredoc_word;
-	char	*dstfile;
-	int		o_flag;
-}	t_exec_info;
-
-typedef struct s_lexer_manager
-{
-	t_token		*token;
-	int			token_index;
-	int			word_index;
-	char		word[4097];
-	int			state;
-	bool		is_misuse_builtin;
-}	t_lexer_manager;
-
-enum e_lexer_state
-{
-	NEUTRAL,
-	IN_CBRACKET,
-	IN_SQUOTE,
-	IN_DQUOTE,
-	IN_BQUOTE
-};
 
 #endif
--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 16:29:44 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/05/14 16:17:02 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/05/15 18:24:02 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include "mlx.h"
 # include "xlibft.h"
-# include "vector3.h"
-# include "math.h"
+# include "define.h"
+
 
 # define H 1001
 # define W 1001
@@ -36,5 +36,63 @@ typedef struct s_window_info {
 	void		*win;
 	t_image		*img;
 }	t_window_info;
+
+/****************************************************************
+ * 複数個出現するオブジェクトのみenumとして定義する。
+ * ******/
+enum e_object
+{
+	T_NOTUSE,
+	T_SPHERE,
+	T_PLANE,
+	T_CYLINDER,
+};
+
+/****************************************************************
+ * 線形リストで、t_objectを持つことで複数オブジェクトのオブジェクト一覧を保持する
+ * オブジェクトの種類によって型が違うのでidentifierで区別してからptrをキャストする。
+ * ******/
+typedef struct s_object {
+	enum e_object	identifier;
+	void			*ptr;
+}	t_object;
+
+typedef struct s_ambient_lightning {
+	double		lighting_ratio;
+	int			color;
+}	t_ambient_lightning;
+
+typedef struct s_camera {
+	t_vec3		view_point;
+	t_vec3		orientation_vector;
+	double		fov;
+}	t_camera;
+
+typedef struct s_light {
+	t_vec3		light_point;
+	double		brightness_ratio;
+	t_vec3		orientation_vector;
+	int			color;
+}	t_light;
+
+typedef struct s_sphere {
+	t_vec3		sphere_center;
+	double		diameter;
+	int			color;
+}	t_sphere;
+
+typedef struct s_plane {
+	t_vec3		coordinates;
+	t_vec3		orientation_vector;
+	int			color;
+}	t_plane;
+
+typedef struct s_cylinder {
+	t_vec3		coordinates;
+	t_vec3		orientation_vector;
+	double		cylinder_diameter;
+	double		cylinder_height;
+	int			color;
+}	t_cylinder;
 
 #endif
