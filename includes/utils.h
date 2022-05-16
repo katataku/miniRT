73d4ff91189e9c152b3e6778f7c66b4ad24aec13
hayashi-ay <go12limchangyong@gmail.com>
2022-05-16 11:53:56 +0900

    fix: header fileのnorm対応

--- includes/utils.h
+++ includes/utils.h
@@ -29,8 +29,8 @@ void	puterr(char *target, char *message);
 void	puterr_exit(char *target, char *message, int exit_status);
 char	*triple_join(char *s1, char *s2, char *s3);
 void	pixel_put_to_image(t_image *data, int x, int y, int color);
-int	get_trgb(int color, int trgb_type);
-int	make_color_from_trgb(int t, int r, int g, int b);
-int	add_color(int a, int b);
+int		get_trgb(int color, int trgb_type);
+int		make_color_from_trgb(int t, int r, int g, int b);
+int		add_color(int a, int b);
 
 #endif
