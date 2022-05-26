b05aeab3304f82aa02bf685d8181511d44d9b1f9
hayashi-ay <go12limchangyong@gmail.com>
2022-05-26 11:03:21 +0900

    fix: 変数名を変更。反射係数(reflection)は物体の色を255で割ったもの

--- srcs/objects/object.c
+++ srcs/objects/object.c
@@ -6,38 +6,38 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 14:15:28 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/05/24 16:27:41 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/05/26 10:55:45 by ahayashi         ###   ########.jp       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "objects.h"
 
-static int	calc_color(int color, double ratio, int reflection)
+static int	calc_color(int color, double ratio, int object_color)
 {
 	return (make_color_from_trgb(\
 		0xFF, \
-		get_trgb(color, RED) * ratio * get_trgb(reflection, RED) / 255, \
-		get_trgb(color, GREEN) * ratio * get_trgb(reflection, GREEN) / 255, \
-		get_trgb(color, BLUE) * ratio * get_trgb(reflection, BLUE) / 255 \
+		get_trgb(color, RED) * ratio * get_trgb(object_color, RED) / 255, \
+		get_trgb(color, GREEN) * ratio * get_trgb(object_color, GREEN) / 255, \
+		get_trgb(color, BLUE) * ratio * get_trgb(object_color, BLUE) / 255 \
 	));
 }
 
 int	calc_diffuse_light(t_ray *ray, t_object *object, t_light *light)
 {
 	double	cos;
-	double	reflection;
+	double	object_color;
 
 	if (object->type == T_PLANE)
 	{
 		cos = calc_lambert_cos_plane(ray, object->ptr, light);
-		reflection = ((t_plane *)(object->ptr))->color;
+		object_color = ((t_plane *)(object->ptr))->color;
 	}
 	else
 	{
 		cos = calc_lambert_cos_sphere(ray, object->ptr, light);
-		reflection = ((t_sphere *)(object->ptr))->color;
+		object_color = ((t_sphere *)(object->ptr))->color;
 	}
-	return (calc_color(light->color, light->ratio * cos, reflection));
+	return (calc_color(light->color, light->ratio * cos, object_color));
 }
 
 int	calc_ambient_light(t_ambient_light *a, t_object *object)
